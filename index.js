0d7a6f38613f66e751e7d90c0a9ac400f6b29f73
yangyimin <yangyimin@xinxindai.com>
2019-04-23 18:04:02 +0800

    koa路由

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const Koa = require('koa')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+const Router = require('koa-router')
 
 const app = new Koa()
-const {connect, initSchemas} = require('./database/init.js')
+//装载所有子路由
+let router = new Router()
 
-;(async () => {
-	await connect()
-	initSchemas()
-	let User = mongoose.model('User')
-	let oneuser = new User({userName: 'yangyimin', password: '123456'})
-	oneuser.save().then(() => {
-		console.log('保存成功')
-	})
-	let oneSS = await User.findOne({}).exec()
-	console.log(`********* ${oneSS} ************`)
-})()
+let user = require('./appApi/user.js')
+router.use('/user', user.routes())
+//egg.js
+
+//加载路由中间件
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+// const {connect, initSchemas} = require('./database/init.js')
+
+// ;(async () => {
+// 	await connect()
+// 	initSchemas()
+// 	let User = mongoose.model('User')
+// 	let oneuser = new User({userName: 'yangyimin3', password: '123456'})
+// 	oneuser.save().then(() => {
+// 		console.log('保存成功')
+// 	})
+// 	let oneSS = await User.findOne({}).exec()
+// 	console.log(`********* ${oneSS} ************`)
+// })()
 
 app.use(async ctx => {
 	ctx.body = 'hahaha'
